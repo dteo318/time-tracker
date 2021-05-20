123ef8af34bdac9273ee03cb8d1b33d250e2f7aa
Daniel Teo <dteo318@gmail.com>
2021-05-19 22:03:13 -0700

    Configured schedule html file

--- tracker/tracker_site/views.py
+++ tracker/tracker_site/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def schedule_view(request):
-    return HttpResponse("<h1>Hello</h1>")
+    return render(request, 'tracker_site/schedule.html')
