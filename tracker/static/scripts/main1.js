96bbc766434419c75b9913e5c237c57d3f07051b
Daniel Teo <dteo318@gmail.com>
2021-06-01 23:50:09 -0700

    Dates/times set to current date/time by default

--- tracker/static/scripts/main1.js
+++ tracker/static/scripts/main1.js
@@ -10,6 +10,22 @@ const add_event_modal_close_button = document.getElementById(
 add_event_icon.addEventListener("click", function () {
   const modal_div = document.getElementById("add-event-modal-div");
   modal_div.classList.add("is-active");
+
+  // Set modal date selector to current date
+  const day_date = document.getElementById("day-date");
+  const add_event_modal_date_select = document.getElementById("event-date");
+  add_event_modal_date_select.value = day_date.value;
+
+  // Set modal time selector to current time
+  const add_event_modal_time_select = document.getElementById("event-time");
+  let current_time = new Date();
+  const current_hours = current_time.getHours(); // => 9
+  const current_minutes = current_time.getMinutes(); // =>  30
+
+  current_time = current_hours + ":" + current_minutes;
+  add_event_modal_time_select.value = current_time;
+
+  console.log("MODAL CURRENT DATE AND TIME SET");
 });
 
 add_event_modal_cancel_button.addEventListener("click", function () {
@@ -49,3 +65,17 @@ add_event_modal_save_button.addEventListener("click", function () {
   const modal_div = document.getElementById("add-event-modal-div");
   modal_div.classList.remove("is-active");
 });
+
+// Setting date to current date on load
+function setCurrentDayDate() {
+  const day_date = document.getElementById("day-date");
+  let current_date = new Date();
+  const dd = String(current_date.getDate()).padStart(2, "0");
+  const mm = String(current_date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  const yyyy = current_date.getFullYear();
+
+  current_date = yyyy + "-" + mm + "-" + dd;
+
+  day_date.value = current_date;
+  console.log("CURRENT DATE SET!");
+}
